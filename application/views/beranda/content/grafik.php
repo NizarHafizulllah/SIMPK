7d6ca4187604cf8e3ef4888916ece5302a4d7713
erwinmardinata <537.mardinata@gmail.com>
2016-09-15 22:12:48 +0800

    update grafik pada tampilan frontend / Beranda

--- application/views/beranda/content/grafik.php
+++ application/views/beranda/content/grafik.php
@@ -2,63 +2,97 @@
 <hr>
 <script>
 $(function () {
-
-    $(document).ready(function () {
-
-        // Build the chart
-        $('#grafik').highcharts({
-            chart: {
-                plotBackgroundColor: null,
-                plotBorderWidth: null,
-                plotShadow: false,
-                type: 'line'
-            },
+		
+    $('#grafik').highcharts({
+        title: {
+            text: 'Data Jumlah Penduduk Miskin per Kabupaten',
+            x: -20 //center
+        },
+		subtitle: {
+            text: 'Tahun <?php echo $tahun ?>',
+            x: -20
+        },
+        xAxis: {
+            categories: [
+				<?php
+				
+					foreach($kab as $row) {
+						echo "'.$row->nama_kab.'".",";
+					}
+				
+				?>
+			]
+        },
+        yAxis: {
             title: {
-                text: 'Data Jumlah Penduduk Miskin per Kabupaten'
+                text: 'Jumlah Penduduk Miskin Tahun <?php echo $tahun; ?>'
             },
-            tooltip: {
-                pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-            },
-            plotOptions: {
-                pie: {
-                    allowPointSelect: true,
-                    cursor: 'pointer',
-                    dataLabels: {
-                        enabled: false
-                    },
-                    showInLegend: true
-                }
-            },
-            series: [{
-                name: 'Brands',
-                colorByPoint: true,
-                data: [{
-                    name: 'Brang Ene',
-                    y: 56.33
-                }, {
-                    name: 'Brang Re',
-                    y: 24.03,
-                    sliced: true,
-                    selected: true
-                }, {
-                    name: 'Jereweh',
-                    y: 10.38
-                }, {
-                    name: 'Maluk',
-                    y: 4.77
-                }, {
-                    name: 'Poto Tano',
-                    y: 0.91
-                }, {
-                    name: 'Proprietary or Undetectable',
-                    y: 0.2
-                }]
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
             }]
-        });
+        },
+        tooltip: {
+            valueSuffix: ' Orang'
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+        },
+        series: [{
+            name: 'Jumlah',
+            data: [
+				<?php
+					
+					if($jml == null ) {
+						for($x=1; $x<=count($kab); $x++){
+							echo '0, ';
+						}
+					} else {
+						foreach($jml as $row) {
+							echo $row->jumlah.', ';
+						}
+					}
+					
+				?>
+			]
+        }]
     });
+	
+	$('#cari').click(function() {
+		var nilai = $('#tahun').val();
+		
+		if(!nilai) {
+			alert('Anda harus pilih tahun dulu');
+			return false;
+		} 
+	});
+	
 });
-
 </script>
 
-<div id="grafik">
-</div>
+<nav class="navbar navbar-default" role="navigation">
+  <div class="container-fluid">
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1" style="margin-right: 20px;">
+      <form class="navbar-form navbar-right" role="search">
+        <div class="form-group">
+			<select class="form-control" name="tahun" id="tahun">
+				<option value="">- Pilih Tahun -</option>
+				<?php for($x=date('Y'); $x>=2000; $x--) { ?>
+					<option <?php echo $x == $tahun ? 'selected' : ''; ?> value="<?php echo $x; ?>"><?php echo $x; ?></option>
+				<?php } ?>
+			</select>
+		</div>
+        <button type="submit" class="btn btn-default" id="cari">Cari</button>
+      </form>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+<?php if(isset($_GET['tahun'])){ ?>
+	<div id="grafik"></div>
+<?php } ?>
