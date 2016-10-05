66989b075168de97d1892aeb8eeb5d91f4b7fa10
NizarHafizulllah <nizarhafizullah66@gmail.com>
2016-10-05 09:06:58 +0700

    Dashboard Beranda

--- application/views/beranda/content/welcome.php
+++ application/views/beranda/content/welcome.php
@@ -6,12 +6,12 @@
 	  <li><img src="<?php echo base_url('assets/images/d.jpg'); ?>" alt=""></li>
 	</ul>
 <h3><img src="<?php echo base_url('assets/img/ksb.png'); ?>" width="50px" height="auto">
-Selamat datang di SIMPK Sumbawa Barat</h3>
+Selamat datang di SIMPK Kab. Sumbawa Barat</h3>
 <hr>
-Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+Aplikasi SIMPK (Sistem Informasi Manajemen Pengendalian Kemiskinan) Kab. Sumbawa Barat merupakan aplikasi yang dikelola oleh Pemerintah Kabupaten Sumbawa Barat yang dimana berfunsi sebagai media publikasi data masyarakat miskin dikabupaten Sumbawa barat serta perbandingannya dengan Kabupaten-kabupaten lain yang berada di Provinsi Nusa Tenggara Barat.
 <br>
 <br>
-Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+Aplikasi SIMPK ini dibangun pada tahun 2016. dan terdiri dari beberapa menu yaitu <b> Pivot</b>, <b> Profil Daerah</b>, <b>Profil Program</b>, <b>Datamart</b>, <b>Grafik</b>, <b>Foto Kegiatan </b> 
 <br>
 <br>
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+
